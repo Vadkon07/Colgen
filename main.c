caa61072351e026a3cbcf756da6f375756b3abff
Vadkon <vadkon2007@gmail.com>
2024-12-26 09:45:40 -0500

    Update

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Structure to hold color data
 typedef struct {
     GdkRGBA color;
 } ColorData;
 
-void generate_color(GtkWidget *button, gpointer data) {
+void generate_color_HEX(GtkWidget *button, gpointer data) {
     GtkWidget **widgets = (GtkWidget **)data;
     GtkWidget *drawing_area = widgets[0];
     ColorData *color_data = (ColorData *)widgets[1];
@@ -16,7 +17,37 @@ void generate_color(GtkWidget *button, gpointer data) {
     sprintf(hex_color, "#%02X%02X%02X", rand() % 256, rand() % 256, rand() % 256);
     gdk_rgba_parse(&(color_data->color), hex_color);
 
-    g_print("Generated Color: %s\n", hex_color); // Print the generated color
+    g_print("Generated Color HEX: %s\n", hex_color); // Print the generated color
+
+    gtk_widget_queue_draw(drawing_area);
+}
+
+void generate_color_RGB(GtkWidget *button, gpointer data) {
+    GtkWidget **widgets = (GtkWidget **)data;
+    GtkWidget *drawing_area = widgets[0];
+    ColorData *color_data = (ColorData *)widgets[1];
+
+    // Generate a random color
+    char rgb_color[32];
+    sprintf(rgb_color, "rgb(%d, %d, %d)", rand() % 256, rand() % 256, rand() % 256);
+    gdk_rgba_parse(&(color_data->color), rgb_color);
+
+    g_print("Generated Color RGB: %s\n", rgb_color); // Print the generated color
+
+    gtk_widget_queue_draw(drawing_area);
+}
+
+void generate_color_RGBA(GtkWidget *button, gpointer data) {
+    GtkWidget **widgets = (GtkWidget **)data;
+    GtkWidget *drawing_area = widgets[0];
+    ColorData *color_data = (ColorData *)widgets[1];
+
+    // Generate a random color
+    char rgba_color[32];
+    snprintf(rgba_color, sizeof(rgba_color), "rgba(%d, %d, %d, %0.2f)", rand() % 256, rand() % 256, rand() % 256, (float)(rand() % 101) / 100);
+    gdk_rgba_parse(&(color_data->color), rgba_color);
+
+    g_print("Generated Color RGBA: %s\n", rgba_color); // Print the generated color
 
     gtk_widget_queue_draw(drawing_area);
 }
@@ -36,6 +67,10 @@ int main(int argc, char *argv[]) {
     GtkWidget *generate_button;
     GtkWidget *format_choice;
     GtkWidget *drawing_area;
+    GtkWidget *generate_button_hex;
+    GtkWidget *generate_button_rgb;
+    GtkWidget *generate_button_rgba;
+
     ColorData color_data = { {0, 0, 0, 1} };  // Initialize with black color
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -61,14 +96,27 @@ int main(int argc, char *argv[]) {
 
     generate_button = gtk_button_new_with_label("Generate Color");
 
+    generate_button_hex = gtk_button_new_with_label("Generare HEX Color");
+    generate_button_rgb = gtk_button_new_with_label("Generate RGB Color");
+    generate_button_rgba = gtk_button_new_with_label("Generate RGBA Color");
+
     drawing_area = gtk_drawing_area_new();
     gtk_widget_set_size_request(drawing_area, 200, 100);  // Set a size for the drawing area
     gtk_box_pack_start(GTK_BOX(box), drawing_area, TRUE, TRUE, 0);
 
     // Create an array to hold the widgets for passing to the callback
     GtkWidget *widgets[] = {drawing_area, (GtkWidget *)&color_data};
-    g_signal_connect(generate_button, "clicked", G_CALLBACK(generate_color), widgets);
+    g_signal_connect(generate_button, "clicked", G_CALLBACK(generate_color_HEX), widgets); 
     gtk_box_pack_start(GTK_BOX(box), generate_button, FALSE, FALSE, 5);
+
+    g_signal_connect(generate_button_hex, "clicked", G_CALLBACK(generate_color_HEX), widgets);
+    gtk_box_pack_start(GTK_BOX(box), generate_button_hex, FALSE, FALSE, 5);
+
+    g_signal_connect(generate_button_rgb, "clicked", G_CALLBACK(generate_color_RGB), widgets);
+    gtk_box_pack_start(GTK_BOX(box), generate_button_rgb, FALSE, FALSE, 5);
+
+    g_signal_connect(generate_button_rgba, "clicked", G_CALLBACK(generate_color_RGBA), widgets);
+    gtk_box_pack_start(GTK_BOX(box), generate_button_rgba, FALSE, FALSE, 5);
     
     g_signal_connect(drawing_area, "draw", G_CALLBACK(draw_callback), &color_data);
 
