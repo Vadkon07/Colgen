005339b05b256784cbe1ac9eaadaa36d8675313a
Vadkon <vadkon2007@gmail.com>
2024-12-27 13:07:38 -0500

    Update

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ typedef struct {
 void generate_color_HEX(GtkWidget *button, gpointer data) {
     GtkWidget **widgets = (GtkWidget **)data;
     GtkWidget *drawing_area = widgets[0];
-    ColorData *color_data = (ColorData *)widgets[1];
+    GtkWidget *text_color = widgets[1];
+    ColorData *color_data = (ColorData *)widgets[2];
 
     // Generate a random color
     char hex_color[8];
@@ -18,6 +19,7 @@ void generate_color_HEX(GtkWidget *button, gpointer data) {
     gdk_rgba_parse(&(color_data->color), hex_color);
 
     g_print("Generated Color HEX: %s\n", hex_color); // Print the generated color
+    gtk_label_set_text(GTK_LABEL(text_color), hex_color);
 
     gtk_widget_queue_draw(drawing_area);
 }
@@ -25,7 +27,8 @@ void generate_color_HEX(GtkWidget *button, gpointer data) {
 void generate_color_RGB(GtkWidget *button, gpointer data) {
     GtkWidget **widgets = (GtkWidget **)data;
     GtkWidget *drawing_area = widgets[0];
-    ColorData *color_data = (ColorData *)widgets[1];
+    GtkWidget *text_color = widgets[1];
+    ColorData *color_data = (ColorData *)widgets[2];
 
     // Generate a random color
     char rgb_color[32];
@@ -33,6 +36,7 @@ void generate_color_RGB(GtkWidget *button, gpointer data) {
     gdk_rgba_parse(&(color_data->color), rgb_color);
 
     g_print("Generated Color RGB: %s\n", rgb_color); // Print the generated color
+    gtk_label_set_text(GTK_LABEL(text_color), rgb_color);
 
     gtk_widget_queue_draw(drawing_area);
 }
@@ -40,7 +44,8 @@ void generate_color_RGB(GtkWidget *button, gpointer data) {
 void generate_color_RGBA(GtkWidget *button, gpointer data) {
     GtkWidget **widgets = (GtkWidget **)data;
     GtkWidget *drawing_area = widgets[0];
-    ColorData *color_data = (ColorData *)widgets[1];
+    GtkWidget *text_color = widgets[1];
+    ColorData *color_data = (ColorData *)widgets[2];
 
     // Generate a random color
     char rgba_color[32];
@@ -48,6 +53,7 @@ void generate_color_RGBA(GtkWidget *button, gpointer data) {
     gdk_rgba_parse(&(color_data->color), rgba_color);
 
     g_print("Generated Color RGBA: %s\n", rgba_color); // Print the generated color
+    gtk_label_set_text(GTK_LABEL(text_color), rgba_color);
 
     gtk_widget_queue_draw(drawing_area);
 }
@@ -64,12 +70,11 @@ int main(int argc, char *argv[]) {
 
     GtkWidget *window;
     GtkWidget *box;
-    GtkWidget *generate_button;
-    GtkWidget *format_choice;
     GtkWidget *drawing_area;
     GtkWidget *generate_button_hex;
     GtkWidget *generate_button_rgb;
     GtkWidget *generate_button_rgba;
+    GtkWidget *text_color;
 
     ColorData color_data = { {0, 0, 0, 1} };  // Initialize with black color
 
@@ -86,16 +91,6 @@ int main(int argc, char *argv[]) {
     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), box);
 
-    format_choice = gtk_combo_box_text_new();
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(format_choice), NULL, "None");
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(format_choice), NULL, "HEX (FOR NOW ONLY HEX IS AVAILABLE!)");
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(format_choice), NULL, "RGB");
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(format_choice), NULL, "RGBA");
-    gtk_combo_box_set_active(GTK_COMBO_BOX(format_choice), 0); // Set default to "None"
-    gtk_box_pack_start(GTK_BOX(box), format_choice, FALSE, FALSE, 5);
-
-    generate_button = gtk_button_new_with_label("Generate Color");
-
     generate_button_hex = gtk_button_new_with_label("Generare HEX Color");
     generate_button_rgb = gtk_button_new_with_label("Generate RGB Color");
     generate_button_rgba = gtk_button_new_with_label("Generate RGBA Color");
@@ -104,11 +99,11 @@ int main(int argc, char *argv[]) {
     gtk_widget_set_size_request(drawing_area, 200, 100);  // Set a size for the drawing area
     gtk_box_pack_start(GTK_BOX(box), drawing_area, TRUE, TRUE, 0);
 
-    // Create an array to hold the widgets for passing to the callback
-    GtkWidget *widgets[] = {drawing_area, (GtkWidget *)&color_data};
-    g_signal_connect(generate_button, "clicked", G_CALLBACK(generate_color_HEX), widgets); 
-    gtk_box_pack_start(GTK_BOX(box), generate_button, FALSE, FALSE, 5);
+    text_color = gtk_label_new(" ");
+    gtk_box_pack_start(GTK_BOX(box), text_color, FALSE, FALSE, 5);
 
+    // Create an array to hold the widgets for passing to the callback
+    GtkWidget *widgets[] = {drawing_area, text_color, (GtkWidget *)&color_data};
     g_signal_connect(generate_button_hex, "clicked", G_CALLBACK(generate_color_HEX), widgets);
     gtk_box_pack_start(GTK_BOX(box), generate_button_hex, FALSE, FALSE, 5);
 
